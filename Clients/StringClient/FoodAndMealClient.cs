c224cc74040dff40a0c07d41f193fe7bcfb3b442
raphael-kahler <raphael.kahler@gmail.com>
2019-05-12 17:22:01 -0700

    Add GraphQL client

---
+++ Clients/StringClient/FoodAndMealClient.cs
@@ -0,0 +1,61 @@
+﻿using GraphQL.Client;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class FoodAndMealClient
+    {
+        private readonly GraphQLClient _graphQLClient;
+
+        public FoodAndMealClient(GraphQLClient graphQLClient)
+        {
+            _graphQLClient = graphQLClient;
+        }
+
+        public async Task GetMeal(int id)
+        {
+            Console.WriteLine($"Getting meal with id = {id}");
+            var query = new GraphQLRequest
+            {
+                Query = @"
+                query mealQuery($id: Int!) {
+                    meal(id: $id) {
+                        name
+                        instructions
+                        servingSize
+                        ingredients {
+                            ingredient {
+                                name
+                            }
+                            quantity {
+                                unit
+                                value
+                            }
+                        }
+                    }
+                }",
+                Variables = new { id = id }
+            };
+            var response = await _graphQLClient.PostAsync(query);
+            PrintResponse(response);
+        }
+
+        private void PrintResponse(GraphQLResponse response)
+        {
+            if (response.Errors != null)
+            {
+                Console.WriteLine("Error returned from GraphQL request:");
+                foreach (var error in response.Errors)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+            Console.WriteLine("Data:");
+            Console.WriteLine(response.Data);
+        }
+    }
+}
---
+++ Clients/StringClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using GraphQL.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var graphQLClient = new GraphQLClient("https://localhost:44364/graphql");
+                var client = new FoodAndMealClient(graphQLClient);
+                await client.GetMeal(1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
